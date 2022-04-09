d25587a9207fba3245d4f41143b7ce05072ec04b
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2022-04-08 23:57:32 -1000

    idk

--- app/src/main/java/com/example/haletest/BusinessInfoPageActivity.java
+++ app/src/main/java/com/example/haletest/BusinessInfoPageActivity.java
@@ -1,28 +1,67 @@
 package com.example.haletest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BusinessInfoPageActivity extends AppCompatActivity {
     private ImageView imageView;
     private TextView name;
     private TextView description;
     private TextView location;
+    private ImageButton favButton;
+    String placeId;
+
+    boolean isInMyFavorite = false;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.business_info_page);
+
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        if (firebaseAuth.getCurrentUser() != null) {
+//            checkIsFavorite();
+//        }
         imageView = (ImageView) findViewById(R.id.imageView);
         name = (TextView) findViewById(R.id.name);
         description = (TextView) findViewById(R.id.description);
         location = (TextView) findViewById(R.id.location);
+        favButton = (ImageButton) findViewById(R.id.favButton);
+
+        placeId = name.getText().toString();
+
+        favButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (!isInMyFavorite) {
+                    favButton.setImageResource(R.drawable.favorite_turned_in);
+                    isInMyFavorite = !isInMyFavorite;
+                } else {
+                    favButton.setImageResource(R.drawable.favorite_turned_out);
+                    isInMyFavorite = !isInMyFavorite;
+                }
+            }
+        });
+
         Businesses bus = (Businesses) getIntent().getSerializableExtra(SearchActivity.BUS_DETAIL_KEY);
         loadBusinessPage(bus);
     }
@@ -35,5 +74,26 @@ public class BusinessInfoPageActivity extends AppCompatActivity {
         name.setText(business.getBusinessName());
         description.setText(business.getDescription());
         location.setText(business.getLocation());
+    }
+
+    private void checkIsFavorite() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseAuth.getUid()).child("Favorites").child(placeId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        isInMyFavorite = snapshot.exists();
+                        if (isInMyFavorite) {
+                            favButton.setImageResource(R.drawable.favorite_turned_in);
+                        } else {
+                            favButton.setImageResource(R.drawable.favorite_turned_out);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 }
