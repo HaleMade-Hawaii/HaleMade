483c344a5c46aa0855376cd80e7196db885d3902
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2022-04-10 21:45:26 -1000

    checkpoint - display database

--- app/src/main/java/com/example/haletest/FavoriteActivity.java
+++ app/src/main/java/com/example/haletest/FavoriteActivity.java
@@ -1,71 +1,82 @@
 package com.example.haletest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class FavoriteActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class FavoriteActivity extends AppCompatActivity {
 
     ListView list;
     ListViewAdapter adapter;
-    SearchView editsearch;
-    Businesses[] businesses;
-    ArrayList<Businesses> arrayList = new ArrayList<Businesses>();
-    public static final String BUS_DETAIL_KEY = "business";
+    ArrayList<String> arrayList = new ArrayList<>();
     private FirebaseAuth firebaseAuth;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        list = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(FavoriteActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(myArrayAdapter);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
-        businesses = new Businesses[3];
-        businesses[0] = new Businesses("Hawaii Doggie Bakery",
-                "We are Hawaii’s original bakery for dogs, founded in 1998, handcrafting " +
-                        "fresh baked healthy innovative treat for dogs using quality local Hawaiian " +
-                        "ingredients!\n", "https://i.imgur.com/YfOW14C.jpg", "2961 E Manoa Rd, Honolulu, HI 96822");
-        businesses[1] = new Businesses("Purve Donut Shop",
-                "Life Changing Donuts Made Fresh To Order!", "https://i.imgur.com/Jr15ebj.jpg",
-                "1234 Kona St, Honolulu, HI 96814");
-        businesses[2] = new Businesses("Lanikai Bath & Body",
-                "Made fresh and all natural, Lanikai Bath and Body reflects the Hawaii of today, beautiful, light-hearted and cosmopolitan.",
-                "https://i.imgur.com/QOIfPvJ.png",
-                "600 Kailua Road. No. 119 Kailua, Hawaii 96734");
+        ref = FirebaseDatabase.getInstance().getReference("Users").child(firebaseAuth.getUid()).child("Favorites");
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                HashMap<String, Object> value = (HashMap<String, Object>) snapshot.getValue();
+                String temp = (String) value.get("placeId");
+                arrayList.add(temp);
+                myArrayAdapter.notifyDataSetChanged();
 
-        list = (ListView) findViewById(R.id.listview);
+            }
 
-        arrayList.add(businesses[0]);
-        arrayList.add(businesses[1]);
-        arrayList.add(businesses[2]);
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
-        // Pass results to com.example.haletest.ListViewAdapter Class
-        adapter = new ListViewAdapter(this, arrayList);
-        adapter.checkIsFavorite(firebaseAuth.getUid());
+            }
 
-        // Binds the Adapter to the ListView
-        list.setAdapter(adapter);
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
+            }
 
-        // Locate the EditText in listview_main.xml
-        editsearch = (SearchView) findViewById(R.id.search);
-        editsearch.setOnQueryTextListener(this);
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-        setupBusinessSelectedListener();
+            }
+        });
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -99,30 +110,5 @@ public class FavoriteActivity extends AppCompatActivity implements SearchView.On
             }
         });
 
-    }
-
-    @Override
-    public boolean onQueryTextSubmit (String query){
-
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange (String newText){
-        String text = newText;
-        adapter.filter(text);
-        return false;
-    }
-
-    public void setupBusinessSelectedListener() {
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Launch the detail view passing book as an extra
-                Intent intent = new Intent(FavoriteActivity.this, BusinessInfoPageActivity.class);
-                intent.putExtra(BUS_DETAIL_KEY, adapter.getItem(position));
-                startActivity(intent);
-            }
-        });
     }
 }
